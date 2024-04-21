59153144e6675d3ff740ab10105588153ff3c81d
mohan <mohan14242@gmail.com>
2024-04-21 12:59:13 +0530

    mohan

--- module-vpc/main.tf
+++ module-vpc/main.tf
@@ -14,6 +14,13 @@ provider "aws" {
 
 module "mohan" {
     source = "../terraform-vpc"
-    
+    cidr_block = var.cidr_block
+    project_name = var.project_name 
+    common_tags = var.common_tags
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    database_subnet_cidr = var.database_subnet_cidr
+    env=var.env
+
   
 }
